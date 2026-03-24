f19c2f52b753b37167e28f51f9a1aefff2e5ef33
Ahmed Abdelnasser <ahmedabdelnasser.dev@gmail.com>
2026-03-24 19:13:56 +0200

    Built an even Odd App with validation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,33 @@
 fun main() {
-//    cant change the value after init
-    val x = 5
+    val number = validateInput(getInput())
+    printOutput(number)
+}
 
-//    can change the value
-    var y = 10
-    y += 10
+//--------------------------------------------------------------------------------
 
-    println(x+y)
+// Getting the User input
+fun getInput(): Int? {
+    println("Please enter a number")
+    return readln().toIntOrNull()
+}
+
+// Checking of the value is a true number or not
+fun validateInput(input: Int?): Int {
+    return if (input == null) {
+        println("Invalid input. Please try again.")
+        validateInput(getInput())
+    } else {
+        println("Your number is $input and is a valid integer")
+        input
+    }
+}
+
+// Check if the number is even or odd
+fun isEven(num: Int): String {
+    return if (num % 2 == 0) "Even" else "Odd"
+}
+
+// Printing out the output
+fun printOutput(num: Int) {
+    println("Your number is $num and it's: ${isEven(num)}")
 }
