82b3f2c92621590a1ee973b3cf93f4f071ce4460
Ahmed Abdelnasser <ahmedabdelnasser.dev@gmail.com>
2026-03-24 19:13:56 +0200

    Built an even Odd App with validation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,17 @@
 fun main() {
-    val number = validateInput(getInput())
-    printOutput(number)
-}
 
-//--------------------------------------------------------------------------------
+    println("How many numbers will you enter? ")
+    val amountOfNumbers: Int = readln().toIntOrNull() ?: 0
+    val numbers = mutableSetOf<Int>()
+    
+    var i = 0
+    while (i < amountOfNumbers) {
 
-// Getting the User input
-fun getInput(): Int? {
-    println("Please enter a number")
-    return readln().toIntOrNull()
-}
-
-// Checking of the value is a true number or not
-fun validateInput(input: Int?): Int {
-    return if (input == null) {
-        println("Invalid input. Please try again.")
-        validateInput(getInput())
-    } else {
-        println("Your number is $input and is a valid integer")
-        input
+        println("Please enter number #${i + 1}")
+        val number = readln().toIntOrNull() ?: continue
+        numbers.add(number)
+        i++
     }
-}
 
-// Check if the number is even or odd
-fun isEven(num: Int): String {
-    return if (num % 2 == 0) "Even" else "Odd"
-}
-
-// Printing out the output
-fun printOutput(num: Int) {
-    println("Your number is $num and it's: ${isEven(num)}")
+    println("The total Sum is: $numbers")
 }
