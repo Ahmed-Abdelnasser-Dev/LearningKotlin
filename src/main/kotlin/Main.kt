6479f3481ac13aa4f2d89656d672976271c01bcf
Ahmed Abdelnasser <ahmedabdelnasser.dev@gmail.com>
2026-03-27 13:07:29 +0200

    Functions, Extension Functions, Functions Overloading, Lambda Functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,38 @@
 fun main() {
 
-    println("How many numbers will you enter? ")
-    val amountOfNumbers: Int = readln().toIntOrNull() ?: 0
-    val numbers = mutableSetOf<Int>()
-    
-    var i = 0
-    while (i < amountOfNumbers) {
+    println("Enter a word to reverse:")
+    val word = readLine()
 
-        println("Please enter number #${i + 1}")
-        val number = readln().toIntOrNull() ?: continue
-        numbers.add(number)
-        i++
-    }
+    // Lambda Function
+    var lettersOnly = word?.filter { character -> character.isLetter() }
 
-    println("The total Sum is: $numbers")
+    println(word?.reversed())
 }
+
+// Normal Functions
+fun reverse(word: String): String {
+    val result = buildString {
+
+        for (char in word.lastIndex downTo 0) {
+            append(word[char])
+        }
+    }
+    return result
+}
+
+// Extension Function
+fun String.reversed(): String {
+    val result = buildString {
+
+        for (char in this@reversed.lastIndex downTo 0) {
+            append(this@reversed[char])
+        }
+    }
+    return result
+}
+
+// Function Overloading
+fun Int.reversed(): Int {
+    return this.toString().reversed().toInt()
+}
+
